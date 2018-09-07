155c6c4c05c3420579eb3d7b7b4696e2163bed61
Adam Shurson <shurson.adam@gmail.com>
2018-09-07 10:29:23 -0500

    UI changes

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -31,28 +31,106 @@ class Home extends Component {
                         options={options}
                     />
                 </div>
-                <div onClick={() => this.props.setPage('detail')} className={"flex w-full lg:w-1/2 xl:w-1/3 bg-white items-center px-4 py-2 my-4 shadow-md hover:shadow-lg cursor-pointer rounded text-teal"}>
-                    <div className={"flex flex-1 flex-col"}>
-                        <div className={"pb-2 flex"}>
-                            <h5 className={"font-normal"}>9/15/2018</h5>
-                            <h5 className={"font-normal ml-auto"}>$500</h5>
+                <div className="flex flex-wrap mt-2">
+                    <div className={"flex w-full lg:w-1/2 xl:w-1/3 py-2 px-1"}>
+                        <div onClick={() => this.props.setPage('detail')} className={"flex w-full bg-white items-center px-4 shadow-md hover:shadow-lg cursor-pointer rounded text-teal"}>
+                            <div className={"flex flex-1 flex-col"}>
+                                <div className={"pb-2 flex"}>
+                                    <h5 className={"font-normal"}>9/15/2018</h5>
+                                    <h5 className={"font-normal ml-auto"}>$500</h5>
+                                </div>
+                                <h5 className={"py-2 font-normal"}>Dr. Jarrod Sheatsley</h5>
+                                <h5 className={"py-2 font-normal"}>Blue Cross Blue Shield</h5>
+                            </div>
+                            <div className={"ml-auto pl-4 py-4 w-1/2 h-32"}>
+                                <GoogleMapReact
+                                    bootstrapURLKeys={{ key: "AIzaSyBYYFXhpIKcxaiDqLDGhJNxs9T9g5Bk4t4" }}
+                                    defaultCenter={this.center}
+                                    defaultZoom={this.zoom}
+                                >
+                                    <AnyReactComponent
+                                        lat={59.955413}
+                                        lng={30.337844}
+                                        text={'Kreyser Avrora'}
+                                    />
+                                </GoogleMapReact>
+                            </div>
                         </div>
-                        <h5 className={"py-2 font-normal"}>Dr. Jarrod Sheatsley</h5>
-                        <h5 className={"py-2 font-normal"}>Tuesday, September 4th @ 10:00am</h5>
-                        <h5 className={"py-2 font-normal"}>Blue Cross Blue Shield PPO</h5>
                     </div>
-                    <div className={"ml-auto pl-4 w-1/2 h-32"}>
-                        <GoogleMapReact
-                            bootstrapURLKeys={{ key: "AIzaSyBYYFXhpIKcxaiDqLDGhJNxs9T9g5Bk4t4" }}
-                            defaultCenter={this.center}
-                            defaultZoom={this.zoom}
-                        >
-                            <AnyReactComponent
-                                lat={59.955413}
-                                lng={30.337844}
-                                text={'Kreyser Avrora'}
-                            />
-                        </GoogleMapReact>
+                    <div className={"flex w-full lg:w-1/2 xl:w-1/3 py-2 px-1"}>
+                        <div onClick={() => this.props.setPage('detail')} className={"flex w-full bg-white items-center px-4 shadow-md hover:shadow-lg cursor-pointer rounded text-teal"}>
+                            <div className={"flex flex-1 flex-col"}>
+                                <div className={"pb-2 flex"}>
+                                    <h5 className={"font-normal"}>9/15/2018</h5>
+                                    <h5 className={"font-normal ml-auto"}>$500</h5>
+                                </div>
+                                <h5 className={"py-2 font-normal"}>Dr. Jarrod Sheatsley</h5>
+                                <h5 className={"py-2 font-normal"}>Blue Cross Blue Shield</h5>
+                            </div>
+                            <div className={"ml-auto pl-4 py-4 w-1/2 h-32"}>
+                                <GoogleMapReact
+                                    bootstrapURLKeys={{ key: "AIzaSyBYYFXhpIKcxaiDqLDGhJNxs9T9g5Bk4t4" }}
+                                    defaultCenter={this.center}
+                                    defaultZoom={this.zoom}
+                                >
+                                    <AnyReactComponent
+                                        lat={59.955413}
+                                        lng={30.337844}
+                                        text={'Kreyser Avrora'}
+                                    />
+                                </GoogleMapReact>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={"flex w-full lg:w-1/2 xl:w-1/3 py-2 px-1"}>
+                        <div onClick={() => this.props.setPage('detail')} className={"flex w-full bg-white items-center px-4 shadow-md hover:shadow-lg cursor-pointer rounded text-teal"}>
+                            <div className={"flex flex-1 flex-col"}>
+                                <div className={"pb-2 flex"}>
+                                    <h5 className={"font-normal"}>9/15/2018</h5>
+                                    <h5 className={"font-normal ml-auto"}>$500</h5>
+                                </div>
+                                <h5 className={"py-2 font-normal"}>Dr. Jarrod Sheatsley</h5>
+                                <h5 className={"py-2 font-normal"}>Blue Cross Blue Shield</h5>
+                            </div>
+                            <div className={"ml-auto pl-4 py-4 w-1/2 h-32"}>
+                                <GoogleMapReact
+                                    bootstrapURLKeys={{ key: "AIzaSyBYYFXhpIKcxaiDqLDGhJNxs9T9g5Bk4t4" }}
+                                    defaultCenter={this.center}
+                                    defaultZoom={this.zoom}
+                                >
+                                    <AnyReactComponent
+                                        lat={59.955413}
+                                        lng={30.337844}
+                                        text={'Kreyser Avrora'}
+                                    />
+                                </GoogleMapReact>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={"flex w-full lg:w-1/2 xl:w-1/3 py-2 px-1"}>
+                        <div onClick={() => this.props.setPage('detail')} className={"flex w-full bg-white items-center px-4 shadow-md hover:shadow-lg cursor-pointer rounded text-teal"}>
+                            <div className={"flex flex-1 flex-col"}>
+                                <div className={"pb-2 flex"}>
+                                    <h5 className={"font-normal"}>9/15/2018</h5>
+                                    <h5 className={"font-normal ml-auto"}>$500</h5>
+                                </div>
+                                <h5 className={"py-2 font-normal"}>Dr. Jarrod Sheatsley</h5>
+                                <h5 className={"py-2 font-normal"}>Blue Cross Blue Shield</h5>
+                            </div>
+                            <div className={"ml-auto pl-4 py-4 w-1/2 h-32"}>
+                                <GoogleMapReact
+                                    bootstrapURLKeys={{ key: "AIzaSyBYYFXhpIKcxaiDqLDGhJNxs9T9g5Bk4t4" }}
+                                    defaultCenter={this.center}
+                                    defaultZoom={this.zoom}
+                                >
+                                    <AnyReactComponent
+                                        lat={59.955413}
+                                        lng={30.337844}
+                                        text={'Kreyser Avrora'}
+                                    />
+                                </GoogleMapReact>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
