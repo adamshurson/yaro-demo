199707a9366a40f1696cba4e9a3fb1b6ca050c27
Shurson, Adam M <adam.m.shurson@accenture.com>
2018-09-10 18:12:44 -0400

    set map size to fill home tabs

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -62,9 +62,9 @@ class Home extends Component {
                 <div className="flex flex-wrap mt-2">
                     {
                         this.state.visits.map((visit) => {
-                            return <div key={visit._id} className={"flex w-full lg:w-1/2 xl:w-1/3 py-2 px-1"}>
-                                <div onClick={() => this.props.setPage('detail')} className={"flex w-full bg-white items-center px-4 shadow-md hover:shadow-lg cursor-pointer rounded text-teal"}>
-                                    <div className={"flex flex-1 flex-col"}>
+                            return <div key={visit._id} className={"flex w-full lg:w-1/2 xl:w-1/3 p-4"}>
+                                <div onClick={() => this.props.setPage('detail')} className={"flex w-full bg-white items-center shadow-md hover:shadow-lg cursor-pointer rounded text-teal"}>
+                                    <div className={"flex flex-1 px-4 flex-col"}>
                                         <div className={"pb-2 flex"}>
                                             <h5 className={"font-normal"}>{(new Date(visit.date)).toLocaleDateString()}</h5>
                                             <h5 className={"font-normal ml-auto"}>${this.calculateCost(visit)}</h5>
@@ -72,7 +72,7 @@ class Home extends Component {
                                         <h5 className={"py-2 font-normal"}>{visit.doctor.profile.first_name + " " + visit.doctor.profile.last_name}</h5>
                                         <h5 className={"py-2 font-normal"}>Blue Cross Blue Shield</h5>
                                     </div>
-                                    <div className={"ml-auto pl-4 py-4 w-1/2 h-32"}>
+                                    <div className={"w-1/2 h-32 rounded-r"}>
                                         <GoogleMapReact
                                             bootstrapURLKeys={{ key: "AIzaSyBYYFXhpIKcxaiDqLDGhJNxs9T9g5Bk4t4" }}
                                             defaultCenter={
