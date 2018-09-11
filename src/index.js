28e4d546af036f0e70f49a77030633a357e17f3a
adamshurson <shurson.adam@gmail.com>
2018-09-10 21:38:08 -0400

    adding unregister for existing users

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // removing because of https
 // registerServiceWorker();
+unregister();
