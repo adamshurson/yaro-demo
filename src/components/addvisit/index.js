406c91295a7a536c7f2f40238641f671e5d9e2b0
Shurson, Adam M <adam.m.shurson@accenture.com>
2018-09-11 22:15:02 -0400

    added scrolling for visits

--- src/components/addvisit/index.js
+++ src/components/addvisit/index.js
@@ -115,41 +115,43 @@ class AddStoredProcedure extends Component {
                         <h4>Create a Visit</h4>
                         <FontAwesomeIcon onClick={() => this.props.exitAddingVisit()} className="cursor-pointer ml-auto fa-2x" icon={"times"} />
                     </div>
-                    <div className="flex flex-col flex-1 overflow-y-auto">
-                        <select value={this.state.doctor} onChange={(evt) => this.setState({doctor: evt.target.value})} className={"px-4 py-2 text-teal my-2 rounded border-2 border-teal focus:outline-none"}>
-                            <option value={0}>Select a Doctor</option>
-                            {
-                                this.state.doctors.map((doc) => {
-                                    return <option key={doc._id} value={doc._id}>{doc.profile.first_name + " " + doc.profile.last_name}</option>
-                                })
-                            }
-                        </select>
-                        <select value={this.state.location.uid} onChange={(evt) => this.setLocation(evt.target.value)} className={"px-4 py-2 text-teal my-2 rounded border-2 border-teal focus:outline-none"}>
-                            <option value={0}>Select a Location</option>
-                            {
-                                this.getDoctor().practices.map((practice) => {
-                                    return <option key={practice.uid} value={practice.uid}>{practice.visit_address.street + " " + practice.visit_address.state + ", " + practice.visit_address.zip}</option>
-                                })
-                            }
-                        </select>
-                        <input value={this.state.date} onChange={(evt) => this.setState({date: evt.target.value})} type={"date"} className={"px-4 py-2 text-teal my-2 rounded border-2 border-teal focus:outline-none"}/>
-                        {
-                            this.state.procedures.map((proc, index) => {
-                                return <div key={index} className="flex">
-                                    <p className={"flex-1 px-4 py-2 text-teal my-2 rounded border-2 border-teal focus:outline-none"}>{proc.name} cost:</p>
-                                    <input value={proc.cost} onChange={(evt) => this.setCost(index, evt.target.value)} type={"number"} step={1} className={"ml-8 px-4 py-2 text-teal my-2 rounded border-2 border-teal focus:outline-none"}/>
-                                </div>
-                            })
-                        }
-                        <div className="flex">
-                            <select value={this.state.currentProcedureIndex} onChange={(evt) => this.setState({currentProcedureIndex: evt.target.value})} className={"flex-1 px-4 py-2 text-teal my-2 rounded border-2 border-teal focus:outline-none"}>
+                    <div className="flex-1 overflow-y-auto">
+                        <div className="flex flex-col h-fit">
+                            <select value={this.state.doctor} onChange={(evt) => this.setState({doctor: evt.target.value})} className={"px-4 py-2 text-teal my-2 rounded border-2 border-teal focus:outline-none"}>
+                                <option value={0}>Select a Doctor</option>
                                 {
-                                    this.state.stored_procedures.map((procedure, index) => {
-                                        return <option key={procedure._id} value={index}>{procedure.name}</option>
+                                    this.state.doctors.map((doc) => {
+                                        return <option key={doc._id} value={doc._id}>{doc.profile.first_name + " " + doc.profile.last_name}</option>
                                     })
                                 }
                             </select>
-                            <button onClick={() => this.addProcedure()} className={"ml-8 px-4 py-2 text-teal my-2 rounded border-2 border-teal focus:outline-none"}>Add Procedure</button>
+                            <select value={this.state.location.uid} onChange={(evt) => this.setLocation(evt.target.value)} className={"px-4 py-2 text-teal my-2 rounded border-2 border-teal focus:outline-none"}>
+                                <option value={0}>Select a Location</option>
+                                {
+                                    this.getDoctor().practices.map((practice) => {
+                                        return <option key={practice.uid} value={practice.uid}>{practice.visit_address.street + " " + practice.visit_address.state + ", " + practice.visit_address.zip}</option>
+                                    })
+                                }
+                            </select>
+                            <input value={this.state.date} onChange={(evt) => this.setState({date: evt.target.value})} type={"date"} className={"px-4 py-2 text-teal my-2 rounded border-2 border-teal focus:outline-none"}/>
+                            {
+                                this.state.procedures.map((proc, index) => {
+                                    return <div key={index} className="flex">
+                                        <p className={"flex-1 px-4 py-2 text-teal my-2 rounded border-2 border-teal focus:outline-none"}>{proc.name} cost:</p>
+                                        <input value={proc.cost} onChange={(evt) => this.setCost(index, evt.target.value)} type={"number"} step={1} className={"ml-8 px-4 py-2 text-teal my-2 rounded border-2 border-teal focus:outline-none"}/>
+                                    </div>
+                                })
+                            }
+                            <div className="flex">
+                                <select value={this.state.currentProcedureIndex} onChange={(evt) => this.setState({currentProcedureIndex: evt.target.value})} className={"flex-1 px-4 py-2 text-teal my-2 rounded border-2 border-teal focus:outline-none"}>
+                                    {
+                                        this.state.stored_procedures.map((procedure, index) => {
+                                            return <option key={procedure._id} value={index}>{procedure.name}</option>
+                                        })
+                                    }
+                                </select>
+                                <button onClick={() => this.addProcedure()} className={"ml-8 px-4 py-2 text-teal my-2 rounded border-2 border-teal focus:outline-none"}>Add Procedure</button>
+                            </div>
                         </div>
                     </div>
                     <button onClick={() => this.addVisit()} className="flex p-4 rounded border-2 border-teal justify-center text-teal hover:text-white hover:bg-teal focus:outline-none">Save</button>
